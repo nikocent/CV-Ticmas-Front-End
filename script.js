cf59cff346b76a2c01a73f0819b0c24e91b67acc
nikocent <nicocenteno202@gmail.com>
2023-02-16 16:28:45 -0300

    Header hecho, estilizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API_URL = "https://randomuser.me/api/";
-const API_CONTAINER = document.getElementById("app");
+const API_CONTAINER = document.getElementById("person");
 const API_PIC_CONTAINER = document.querySelector("#profile-pic");
+const API_LOCATION = document.getElementById("person-location");
 
 async function getPerson(){
     await fetch(API_URL)
@@ -11,8 +12,13 @@ async function getPerson(){
         return data.json()})
     .then(person => {
         const {title, first, last} = person.results[0].name;
+        const {city, state, country} = person.results[0].location;
         API_CONTAINER.textContent = `${title}. ${first} ${last}`;
         API_PIC_CONTAINER.src = person.results[0].picture.large;
+        API_LOCATION.textContent = `${city}, ${state}, ${country}.`;
+    })
+    .catch((error) => {
+        console.error("There was an error fetching", error);
     });
 }
 
